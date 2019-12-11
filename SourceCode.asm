60920189623e8fbb641ce6f165b5a3ca48d6ee31
Ehmad <justEhmadSaeed@gmail.com>
2019-12-11 22:37:27 +0500

    📦Initialize Project

--- SourceCode.asm
+++ SourceCode.asm
@@ -1,15 +1,9 @@
-; AddTwo.asm - adds two 32-bit integers.
-; Chapter 3 example
-
-.386
-.model flat,stdcall
-.stack 4096
-ExitProcess proto,dwExitCode:dword
+INLCUDE IRVINE32.inc
+.data
 
 .code
 main proc
-	mov	eax,5				
-	add	eax,6				
+				
 
 	invoke ExitProcess,0
 main endp
