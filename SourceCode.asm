d71ad1ebffcb4ed94010ba0f02a23815737267bb
Ehmad <justEhmadSaeed@gmail.com>
2019-12-13 20:55:59 +0500

    Add Menu + Display Function

--- SourceCode.asm
+++ SourceCode.asm
@@ -1,10 +1,17 @@
-INLCUDE IRVINE32.inc
+INCLUDE IRVINE32.inc
 .data
-
+msg1 byte "### WELCOME TO LIBRARY MANAGEMENT SYSTEM ###", 0dh, 0ah, 0ah,
+		"Choose One of the Following Options:", 0dh, 0ah,0
 .code
+MSG_DISPLAY proto, var: ptr dword
 main proc
-				
+	INVOKE MSG_DISPLAY,addr MSG1
 
 	invoke ExitProcess,0
 main endp
+MSG_DISPLAY PROC USES EDX, VAR: ptr dword
+	MOV EDX, VAR
+	CALL WRITESTRING
+	RET
+	MSG_DISPLAY ENDP
 end main
