d78fac694b80f9ac3ac97c9911ab8b726a873390
Ehmad <justEhmadSaeed@gmail.com>
2019-12-13 20:57:54 +0500

    Add Menu

--- SourceCode.asm
+++ SourceCode.asm
@@ -1,7 +1,12 @@
 INCLUDE IRVINE32.inc
 .data
 msg1 byte "### WELCOME TO LIBRARY MANAGEMENT SYSTEM ###", 0dh, 0ah, 0ah,
-		"Choose One of the Following Options:", 0dh, 0ah,0
+		"Choose One of the Following Options:", 0dh, 0ah,
+		"1: Register a Member", 0dh, 0ah,
+		"2: Unregister a Member", 0dh, 0ah,
+		"3: View Members", 0dh, 0ah,
+		"4: Exit Program", 0dh, 0ah, 0
+		
 .code
 MSG_DISPLAY proto, var: ptr dword
 main proc
