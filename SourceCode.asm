e865b84ea5c10574ad98860484339d6c64f247e6
Ehmad <justEhmadSaeed@gmail.com>
2019-12-16 22:44:08 +0500

    Improved Menu

--- SourceCode.asm
+++ SourceCode.asm
@@ -1,36 +1,36 @@
 INCLUDE IRVINE32.inc
-.data
-msg1 BYTE 0AH, 
-		"	--------------------------------------------", 0dh, 0ah,
-		"	--  WELCOME TO LIBRARY MANAGEMENT SYSTEM  --", 0dh, 0ah,
-		"	--------------------------------------------", 0dh, 0ah, 0ah,
-		"	Choose One of the Following Options:", 0dh, 0ah,
-		"	1-> Register a Member", 0dh, 0ah,
-		"	2-> Unregister a Member", 0dh, 0ah,
-		"	3-> View Members", 0dh, 0ah,
-		"	4-> Add Book", 0dh, 0ah,
-		"	5-> Remove Book", 0dh, 0ah,
-		"	6-> View Books", 0dh, 0ah,
-		"	7-> Exit Program", 0dh, 0ah, 0
-REG_MSG BYTE "	Enter Member's Name to register: ",0
+.DATA
+msg1	 BYTE 0AH, 
+		 BYTE	"	--------------------------------------------", 0dh, 0ah,
+		 BYTE	"	--  WELCOME TO LIBRARY MANAGEMENT SYSTEM  --", 0dh, 0ah,
+		 BYTE	"	--------------------------------------------", 0dh, 0ah, 0ah,
+		 BYTE	"	1-> Register a Member", 0dh, 0ah,
+		 BYTE	"	2-> Unregister a Member", 0dh, 0ah,
+		 BYTE	"	3-> View Members", 0dh, 0ah,
+		 BYTE	"	4-> Add Book", 0dh, 0ah,
+		 BYTE	"	5-> Remove Book", 0dh, 0ah,
+		 BYTE	"	6-> View Books", 0dh, 0ah,
+		 BYTE	"	7-> Exit Program", 0dh, 0ah,
+		 BYTE	"	Choose Your Option : ", 0
+REG_MSG	 BYTE "	Enter Member's Name to register: ",0
 UNREG_MSG BYTE "	Enter Member's Name to unregister: ",0
 VIEW_MEMBERS_MSG BYTE "	Viewing Registered Members: ",0
-ADD_MSG BYTE "	Enter Book Name & Author Name to Add: ", 0dh, 0ah,
+ADD_MSG  BYTE "	Enter Book Name & Author Name to Add: ", 0dh, 0ah,
 			 "	Separated By Comma:",0
-REMOVE_MSG BYTE "	Enter Book Name to Remove: ",0
+REMOVE_MSG	   BYTE "	Enter Book Name to Remove: ",0
 VIEW_BOOKS_MSG BYTE "	Viewing Books in Library: ",0
-EXIT_MSG BYTE "	Exiting Program...",0dh, 0ah,
+EXIT_MSG	   BYTE "	Exiting Program...",0dh, 0ah,
 				"	See you again :')", 0
 
 MEMBERS_FILE BYTE "MEMBERS.txt",0
-BOOKS_FILE BYTE "BOOKS.txt",0
+BOOKS_FILE   BYTE "BOOKS.txt",0
 BUFFER_SIZE = 5000
-filehandle dword ?
+filehandle   DWORD ?
 
 MAX_INPUT = 40
 INPUT_STRING BYTE MAX_INPUT+1 DUP (?)
-buffer_mem byte buffer_size DUP (?)
-buffer_book byte buffer_size DUP (?)
+buffer_mem BYTE buffer_size DUP (?)
+buffer_book BYTE buffer_size DUP (?)
 REGISTER DWORD 1
 UNREGISTER DWORD 2
 VIEW_MEMBERS DWORD 3
@@ -38,10 +38,10 @@ ADD_BOOK DWORD 4
 REMOVE_BOOK DWORD 5
 VIEW_BOOKS DWORD 6
 EXITP DWORD 7	
-.code
-MSG_DISPLAY proto, var: ptr dword
-STRING_INPUT proto, var1: ptr dword
-main proc
+.CODE
+MSG_DISPLAY proto, var: PTR DWORD
+STRING_INPUT proto, var1: PTR DWORD
+main PROC
 	START:
 	INVOKE MSG_DISPLAY,addr MSG1
 	CALL READINT
