cbacfe4073a3e2bee23ec72d5e2b3654e1888df3
Ehmad <justEhmadSaeed@gmail.com>
2019-12-17 13:04:52 +0500

    Successfully Added Functionality

--- SourceCode.asm
+++ SourceCode.asm
@@ -11,26 +11,47 @@ msg1	 BYTE 0AH
 		 BYTE	"	5-> Exit Program", 0dh, 0ah
 		 BYTE	"	Choose Your Option : ", 0
 REG_MSG	 BYTE "	Enter Member's Name to register: ",0
-VIEW_MEMBERS_MSG BYTE "	Viewing Registered Members: ",0
+VIEW_MEMBERS_MSG BYTE 0Ah,"	Viewing Registered Members: ",0AH, 0DH, 0
 ADD_MSG			 BYTE "	Enter Book Name & Author Name to Add: ", 0dh, 0ah,
 			 "	Separated By Comma:",0
-VIEW_BOOKS_MSG BYTE "	Viewing Books in Library: ",0
-EXIT_MSG	   BYTE "	Exiting Program...",0dh, 0ah,
-				"	See you again :')", 0
+VIEW_BOOKS_MSG BYTE 0Ah, "	Viewing Books in Library: ",  0dh, 0ah, 0
+EXIT_MSG	   BYTE 0AH,
+				    "	----------------- ",0dh, 0ah,
+				    "	Exiting Program...",0dh, 0ah,
+					"	See you again  :')",0dh, 0ah,
+					"	------------------", 0
 bool		   DWORD ?
 MEMBERS_FILE   BYTE "MEMBERS.txt",0
 BOOKS_FILE     BYTE "BOOKS.txt",0
 filehandle     DWORD ?
 BUFFER_SIZE = 5000
 MAX_INPUT = 40
-INPUT_STRING BYTE MAX_INPUT+1 DUP (?)
 buffer_mem   BYTE buffer_size DUP (?)
 buffer_book  BYTE buffer_size DUP (?)
 REGISTER	 DWORD 1
-VIEW_MEMBERS DWORD 3
-ADD_BOOK	 DWORD 4
-VIEW_BOOKS	 DWORD 6
-EXITP		 DWORD 7	
+VIEW_MEMBERS DWORD 2
+ADD_BOOK	 DWORD 3
+VIEW_BOOKS	 DWORD 4
+EXITP		 DWORD 5	
+MEMBER_SIZE = 20
+MEMBER1 DB MEMBER_SIZE DUP (?)
+MEMBER2 DB MEMBER_SIZE DUP (?)
+MEMBER3 DB MEMBER_SIZE DUP (?)
+MEMBER4 DB MEMBER_SIZE DUP (?)
+MEMBER5 DB MEMBER_SIZE DUP (?)
+MEMBER6 DB MEMBER_SIZE DUP (?)
+NUM_MEMBERS DWORD 0
+MEMBERS DD MEMBER1, MEMBER2, MEMBER3, MEMBER4, MEMBER5, MEMBER6, 0AH, 0DH, 0
+
+BOOK_SIZE = 30
+BOOK1 DB BOOK_SIZE DUP (?)
+BOOK2 DB BOOK_SIZE DUP (?)
+BOOK3 DB BOOK_SIZE DUP (?)
+BOOK4 DB BOOK_SIZE DUP (?)
+BOOK5 DB BOOK_SIZE DUP (?)
+BOOK6 DB BOOK_SIZE DUP (?)
+NUM_BOOKS DWORD 0
+BOOKS DD BOOK1, BOOK2, BOOK3, BOOK4, BOOK5, BOOK6, 0AH, 0DH, 0
 .CODE
 MSG_DISPLAY proto, var: PTR DWORD
 STRING_INPUT proto, var1: PTR DWORD
@@ -56,38 +77,47 @@ main PROC
 		INVOKE MSG_DISPLAY, ADDR REG_MSG
 		;INVOKE STRING_INPUT, ADDR INPUT_STRING
 
-		INVOKE CreateFile,
-	ADDR MEMBERS_FILE,
-	GENERIC_WRITE,
-	DO_NOT_SHARE, 
-	NULL, 
-	OPEN_ALWAYS, 
-	FILE_ATTRIBUTE_NORMAL, 
-	0
+	MOV ESI, OFFSET MEMBERS
+	MOV EAX, MEMBER_SIZE
+	MUL NUM_MEMBERS
+	ADD ESI, EAX
+	MOV EDX, ESI
+	MOV ECX, MEMBER_SIZE
+	CALL READSTRING
+	INC NUM_MEMBERS
 
-cmp eax, INVALID_HANDLE_VALUE
-	je exit_1
-	mov filehandle, eax
-INVOKE SetFilePointer,
-	 filehandle,
-	0, ; distance low
-	0, ; distance high
-	FILE_END
-	mov eax,filehandle
-
-	mov edx, offset BUFFER_MEM
-	mov ecx, 7
-	call READSTRING
-	mov eax, filehandle
-	call WriteToFile
-
-	INVOKE SetFilePointer,
-	 filehandle,
-	0, ; distance low
-	0, ; distance high
-	FILE_END
-exit_1:
-	invoke CloseHandle, filehandle
+	;INVOKE CreateFile,
+	;ADDR MEMBERS_FILE,
+	;GENERIC_WRITE,
+	;DO_NOT_SHARE, 
+	;NULL, 
+	;OPEN_ALWAYS, 
+	;FILE_ATTRIBUTE_NORMAL, 
+	;0
+;
+;cmp eax, INVALID_HANDLE_VALUE
+	;je exit_1
+	;mov filehandle, eax
+;INVOKE SetFilePointer,
+	 ;filehandle,
+	;0, ; distance low
+	;0, ; distance high
+	;FILE_END
+	;mov eax,filehandle
+;
+	;mov edx, offset BUFFER_MEM
+	;mov ecx, 7
+	;call READSTRING
+	;mov eax, filehandle
+	;call WriteToFile
+;
+	;INVOKE SetFilePointer,
+	 ;filehandle,
+	;0, ; distance low
+	;0, ; distance high
+	;FILE_END
+;exit_1:
+	;invoke CloseHandle, filehandle
 
 		JMP START
 
@@ -96,7 +126,20 @@ exit_1:
 ;--------------------------------------
 	VIEW_M:
 		INVOKE MSG_DISPLAY, ADDR VIEW_MEMBERS_MSG
-
+	MOV ECX, NUM_MEMBERS
+	cmp ECX, 0
+	JE START
+	MOV EBX, 0
+OUTPUT:
+	MOV ESI, OFFSET MEMBERS
+	MOV EAX, MEMBER_SIZE
+	MUL EBX
+	ADD ESI, Eax
+	MOV EDX, ESI
+	CALL WRITESTRING
+	INC EBX
+	CALL CRLF
+LOOP OUTPUT
 		JMP START
 ;----------------------------------
 ;--------------ADD BOOKS-----------
@@ -104,45 +147,35 @@ exit_1:
 	ADD_B:
 		INVOKE MSG_DISPLAY, ADDR ADD_MSG
 		;INVOKE STRING_INPUT, ADDR INPUT_STRING
-INVOKE CreateFile,
-	ADDR BOOKS_FILE,
-	GENERIC_WRITE,
-	DO_NOT_SHARE, 
-	NULL, 
-	OPEN_ALWAYS, 
-	FILE_ATTRIBUTE_NORMAL, 
-	0
-
-cmp eax, INVALID_HANDLE_VALUE
-	je exit_2
-	mov filehandle, eax
-INVOKE SetFilePointer,
-	 filehandle,
-	0, ; distance low
-	0, ; distance high
-	FILE_END
-	mov eax,filehandle
-
-	mov edx, offset BUFFER_BOOK
-	mov ecx, 15
-	call READSTRING
-	mov eax, filehandle
-	call WriteToFile
-
-	INVOKE SetFilePointer,
-	 filehandle,
-	0, ; distance low
-	0, ; distance high
-	FILE_END
-exit_2:
-	invoke CloseHandle, filehandle
+MOV ESI, OFFSET BOOKS
+	MOV EAX, BOOK_SIZE
+	MUL NUM_BOOKS
+	ADD ESI, EAX
+	MOV EDX, ESI
+	MOV ECX, BOOK_SIZE
+	CALL READSTRING
+	INC NUM_BOOKS
 		JMP START
 ;------------------------------------
 ;-------------VIEW BOOKS-------------
 ;------------------------------------
 	VIEW_B:
 		INVOKE MSG_DISPLAY, ADDR VIEW_BOOKS_MSG
-		
+		MOV ECX, NUM_BOOKS
+	cmp ECX, 0
+	JE START
+	MOV EBX, 0
+OUTPUTB:
+	MOV ESI, OFFSET BOOKS
+	MOV EAX, BOOK_SIZE
+	MUL EBX
+	ADD ESI, Eax
+	MOV EDX, ESI
+	CALL WRITESTRING
+	INC EBX
+	CALL CRLF
+	
+LOOP OUTPUTB
 		
 		JMP START
 
