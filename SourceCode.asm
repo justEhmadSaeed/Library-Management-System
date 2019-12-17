c83e2efc7a95be8d9a37d6d6de6cd63605bd28b0
Ehmad <justEhmadSaeed@gmail.com>
2019-12-17 11:34:08 +0500

    Commenting---

--- SourceCode.asm
+++ SourceCode.asm
@@ -1,43 +1,38 @@
 INCLUDE IRVINE32.inc
 .DATA
-msg1	 BYTE 0AH, 
-		 BYTE	"	--------------------------------------------", 0dh, 0ah,
-		 BYTE	"	--  WELCOME TO LIBRARY MANAGEMENT SYSTEM  --", 0dh, 0ah,
-		 BYTE	"	--------------------------------------------", 0dh, 0ah, 0ah,
-		 BYTE	"	1-> Register a Member", 0dh, 0ah,
-		 BYTE	"	2-> Unregister a Member", 0dh, 0ah,
-		 BYTE	"	3-> View Members", 0dh, 0ah,
-		 BYTE	"	4-> Add Book", 0dh, 0ah,
-		 BYTE	"	5-> Remove Book", 0dh, 0ah,
-		 BYTE	"	6-> View Books", 0dh, 0ah,
-		 BYTE	"	7-> Exit Program", 0dh, 0ah,
+msg1	 BYTE 0AH
+		 BYTE	"	--------------------------------------------", 0dh, 0ah
+		 BYTE	"	--  WELCOME TO LIBRARY MANAGEMENT SYSTEM  --", 0dh, 0ah
+		 BYTE	"	--------------------------------------------", 0dh, 0ah, 0ah
+		 BYTE	"	1-> Register a Member", 0dh, 0ah
+		 BYTE	"	2-> View Members", 0dh, 0ah
+		 BYTE	"	3-> Add Book", 0dh, 0ah
+		 BYTE	"	4-> View Books", 0dh, 0ah
+		 BYTE	"	5-> Exit Program", 0dh, 0ah
 		 BYTE	"	Choose Your Option : ", 0
 REG_MSG	 BYTE "	Enter Member's Name to register: ",0
-UNREG_MSG BYTE "	Enter Member's Name to unregister: ",0
 VIEW_MEMBERS_MSG BYTE "	Viewing Registered Members: ",0
-ADD_MSG  BYTE "	Enter Book Name & Author Name to Add: ", 0dh, 0ah,
+ADD_MSG			 BYTE "	Enter Book Name & Author Name to Add: ", 0dh, 0ah,
 			 "	Separated By Comma:",0
-REMOVE_MSG	   BYTE "	Enter Book Name to Remove: ",0
 VIEW_BOOKS_MSG BYTE "	Viewing Books in Library: ",0
 EXIT_MSG	   BYTE "	Exiting Program...",0dh, 0ah,
 				"	See you again :')", 0
-
-MEMBERS_FILE BYTE "MEMBERS.txt",0
-BOOKS_FILE   BYTE "BOOKS.txt",0
+bool		   DWORD ?
+MEMBERS_FILE   BYTE "MEMBERS.txt",0
+BOOKS_FILE     BYTE "BOOKS.txt",0
+filehandle     DWORD ?
 BUFFER_SIZE = 5000
-filehandle   DWORD ?
-
 MAX_INPUT = 40
 INPUT_STRING BYTE MAX_INPUT+1 DUP (?)
-buffer_mem BYTE buffer_size DUP (?)
-buffer_book BYTE buffer_size DUP (?)
-REGISTER DWORD 1
-UNREGISTER DWORD 2
+buffer_mem   BYTE buffer_size DUP (?)
+buffer_book  BYTE buffer_size DUP (?)
+REGISTER	 DWORD 1
+UNREGISTER   DWORD 2
 VIEW_MEMBERS DWORD 3
-ADD_BOOK DWORD 4
-REMOVE_BOOK DWORD 5
-VIEW_BOOKS DWORD 6
-EXITP DWORD 7	
+ADD_BOOK	 DWORD 4
+REMOVE_BOOK  DWORD 5
+VIEW_BOOKS	 DWORD 6
+EXITP		 DWORD 7	
 .CODE
 MSG_DISPLAY proto, var: PTR DWORD
 STRING_INPUT proto, var1: PTR DWORD
@@ -59,6 +54,10 @@ main PROC
 	CMP EAX, VIEW_BOOKS
 	JE VIEW_B
 		JMP EXIT_MENU
+
+;----------------------------------------
+;------------REGISTER MEMBERS------------
+;----------------------------------------
 	REG_M:
 		INVOKE MSG_DISPLAY, ADDR REG_MSG
 		;INVOKE STRING_INPUT, ADDR INPUT_STRING
@@ -98,16 +97,16 @@ exit_1:
 
 		JMP START
 
-; UnRegister Segment
-	UNREG_M:
-		INVOKE MSG_DISPLAY, ADDR UNREG_MSG
-		INVOKE STRING_INPUT, ADDR INPUT_STRING
-		JMP START
+;--------------------------------------
+;--------------VIEW MEMBERS------------
+;--------------------------------------
 	VIEW_M:
 		INVOKE MSG_DISPLAY, ADDR VIEW_MEMBERS_MSG
 
 		JMP START
-		
+;----------------------------------
+;--------------ADD BOOKS-----------
+;----------------------------------	
 	ADD_B:
 		INVOKE MSG_DISPLAY, ADDR ADD_MSG
 		;INVOKE STRING_INPUT, ADDR INPUT_STRING
@@ -135,11 +134,6 @@ INVOKE SetFilePointer,
 	call READSTRING
 	mov eax, filehandle
 	call WriteToFile
-	;INVOKE WriteFile,
-	;filehandle, 
-	;addr buffer,
-	;sizeof buffer,
-	;addr bytesWritten, 0
 
 	INVOKE SetFilePointer,
 	 filehandle,
@@ -149,16 +143,13 @@ INVOKE SetFilePointer,
 exit_2:
 	invoke CloseHandle, filehandle
 		JMP START
-
-	REMOVE_B:
-		INVOKE MSG_DISPLAY, ADDR REMOVE_MSG
-		INVOKE STRING_INPUT, ADDR INPUT_STRING
-		INVOKE MSG_DISPLAY, ADDR INPUT_STRING
-		JMP START
-
+;------------------------------------
+;-------------VIEW BOOKS-------------
+;------------------------------------
 	VIEW_B:
 		INVOKE MSG_DISPLAY, ADDR VIEW_BOOKS_MSG
-
+		
+		
 		JMP START
 
 	EXIT_MENU:
@@ -166,6 +157,9 @@ exit_2:
 	
 	invoke ExitProcess,0
 main endp
+;-------------------------------------------
+;--------FUNCTION TO DISPLAY A STRING-------
+;-------------------------------------------
 MSG_DISPLAY PROC USES EDX, VAR: ptr dword
 	MOV EDX, VAR
 	CALL WRITESTRING
